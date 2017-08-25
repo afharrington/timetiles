b9892e737eeb8c47b90d8c50f55edd73f7935d90
Anna Harrington <annafkh@gmail.com>
2017-08-25 15:39:28 -0400

    Fixed bug with tile name going blank on edit

--- src/components/EntryInstructions/index.js
+++ src/components/EntryInstructions/index.js
@@ -7,7 +7,7 @@ class EntryInstructions extends React.Component {
     return (
       <div className="entry-instructions">
         <div className="entry-instructions-text">
-          <h1>Now add your first entry.</h1>
+          <h1>Add your first entry.</h1>
           <p>Customize the tile color settings by selecting the <FontAwesome name='cog'/> icon above.</p>
           <p>In "Continuous" mode, set how quickly tiles darken and how quickly they fade.</p>
           <p>In "Goal" mode, set a target number of hours per cycle. The tile will be its darkest when you reach the target and reset at the start of a new cycle.</p>
